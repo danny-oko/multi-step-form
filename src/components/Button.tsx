906b689a1b65b6374791d9d0b258c1766f2ad5ef
danny <dnioko0412@gmail.com>
2025-12-23 12:21:00 +0800

    first step: validation

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,7 +4,10 @@ import { useNavigate } from "react-router-dom";
 const Button = () => {
   return (
     <div>
-      <button className="button bg-black cursor-pointer rounded-[6px] text-white w-[416px] h-[44px] ">
+      <button
+        type="submit"
+        className="button bg-black cursor-pointer rounded-[6px] text-white w-[416px] h-[44px] "
+      >
         Continue 1/3
       </button>
     </div>
