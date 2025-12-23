3cd8a29ae71b0db3a16eb402f2dae4c26dae87c2
danny <dnioko0412@gmail.com>
2025-12-23 12:28:32 +0800

    stepper init

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import FirstStep from "./steps/FirstStep.jsx";
 import SecondStep from "./steps/SecondStep.jsx";
 import ThirdStep from "./steps/ThirdStep.jsx";
-
+import Stepper from "./components/Stepper.jsx";
 export default function App() {
   return (
-    <div className="flex items-center justify-between w-full h-[100vh] bg-main">
+    <div className="flex items-center justify-center w-full h-[100vh] bg-main">
       <FirstStep />
-      <SecondStep />
-      <ThirdStep />
+      {/* <SecondStep /> */}
+      {/* <ThirdStep /> */}
+      <Stepper />
     </div>
   );
 }
---
+++ src/components/Stepper.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Stepper = () => {
+  let step = 0;
+  return <div>{step}</div>;
+};
+
+export default Stepper;
