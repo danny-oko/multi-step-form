684e71cb2ecfb503722c3e4f714039c5c02bb367
danny <dnioko0412@gmail.com>
2025-12-25 11:28:05 +0800

    steps: done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { Route, Routes } from "react-router-dom";
 import FirstStep from "./steps/FirstStep.jsx";
 import SecondStep from "./steps/SecondStep.jsx";
 import ThirdStep from "./steps/ThirdStep.jsx";
+import Final from "./steps/Final.jsx";
+const stepper = [FirstStep, SecondStep, ThirdStep];
 
 export default function App() {
   return (
@@ -10,6 +12,7 @@ export default function App() {
         <Route path="/" element={<FirstStep />} />
         <Route path="/second-step" element={<SecondStep />} />
         <Route path="/third-step" element={<ThirdStep />} />
+        <Route path="/final" element={<Final />} />
       </Routes>
     </div>
   );
